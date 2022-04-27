db5cd2b4eaa3ff062f57d4fd3ffea466c42ec61f
sydeich <sydeich@gmail.com>
2022-04-27 15:17:55 -0400

    checklist wowowowowo

--- src/Components/CheckList/CheckList.jsx
+++ src/Components/CheckList/CheckList.jsx
@@ -1,65 +1,30 @@
 import { Link } from "react-router-dom";
 import "../../assets/scss/checklist.scss";
+import Category from "../Categories/Category";
 
 const CheckList = () => {
-	return (
-		<div className="checklist">
-			<div className="top">
-				<div className="left">
-					<h1>Match Preparation</h1>
-				</div>
-				<div className="right">
-					<Link to="/">QR code</Link>
-					<button>Reset</button>
-					<button
-						style={{ backgroundColor: "#D11A2A", marginRight: 0 }}
-					>
-						Delete
-					</button>
-				</div>
-			</div>
-			<div className="bottom">
-				<div className="category">
-					<div className="top">
-						<h1>Category</h1>
-					</div>
-					<div className="bottom">
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-					</div>
-				</div>
-				<div className="category">
-					<div className="top">
-						<h1>Category</h1>
-					</div>
-					<div className="bottom">
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-						<div className="property">
-							<h2>Limelight cover</h2>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="checklist">
+            <div className="top">
+                <div className="left">
+                    <h1>Match Preparation</h1>
+                </div>
+                <div className="right">
+                    <Link to="/">QR code</Link>
+                    <button>Reset</button>
+                    <button
+                        style={{ backgroundColor: "#D11A2A", marginRight: 0 }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
+            <div className="bottom">
+                <Category title="Auto"></Category>
+                <Category title="Teleop"></Category>
+            </div>
+        </div>
+    );
 };
 
 export default CheckList;
