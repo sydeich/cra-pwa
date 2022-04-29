32d3040123806479b47f9a0323ecfcc42d94dd8f
sydeich <sydeich@gmail.com>
2022-04-29 16:12:28 -0400

    hehe

--- src/Components/Popups/NewChecklistPopup.jsx
+++ src/Components/Popups/NewChecklistPopup.jsx
@@ -1,7 +1,30 @@
 import plus from "../../assets/images/plus.svg";
 import "../../assets/scss/newchecklistpopup.scss";
+import { useState } from "react";
+import Database from "../../Database";
 
-const NewChecklistPopup = ({ handleClick }) => {
+const NewChecklistPopup = ({
+    handleClick,
+    database,
+    checklists,
+    setChecklists,
+}) => {
+    const [title, setTitle] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (title == "") {
+            return;
+        }
+        const result = await Database.Checklists.insert({
+            db: database,
+            title: title,
+        });
+        setChecklists([...checklists, { title: title }]);
+        handleClick();
+    };
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
     return (
         <div className="dark-matter">
             <div className="new-checklist">
@@ -9,11 +32,15 @@ const NewChecklistPopup = ({ handleClick }) => {
                     <h1>New Checklist</h1>
                     <img onClick={handleClick} src={plus} alt="" />
                 </div>
-                <div className="bottom">
+                <form onSubmit={handleSubmit} className="bottom">
                     <h1>Title:</h1>
-                    <input type="text" />
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
                     <input type="submit" value="Create" />
-                </div>
+                </form>
             </div>
         </div>
     );
