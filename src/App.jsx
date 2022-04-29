b57cd8450262bf51206aa19acb2eb85f24195af7
sydeich <sydeich@gmail.com>
2022-04-29 16:12:01 -0400

    apple

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as idb from "idb";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import CheckList from "./Components/CheckList/CheckList";
+import Database from "./Database";
 
-function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Header></Header>
-				<div className="left">
-					<Sidebar></Sidebar>
-				</div>
-				<div className="right">
-					<Routes></Routes>
-				</div>
-				<div className="route">
-					<Routes>
-						<Route path="/" element={<CheckList />} />
-					</Routes>
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-}
+const App = () => {
+    const [database, setDatabase] = useState();
+    useEffect(() => {
+        const stuff = async () => {
+            const db = await idb.openDB("checkto", 1, {
+                async upgrade(db, oldVersion, newVersion, transaction) {
+                    const objectStores = ["checklists"];
+                    for (let i = 0; i < objectStores.length; i++) {
+                        try {
+                            await db.deleteObjectStore(objectStores[i]);
+                        } catch (error) {}
+                    }
+                    await Database.first_time(db);
+                },
+            });
+            setDatabase(db);
+        };
+        stuff();
+    }, []);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header></Header>
+                <div className="left">
+                    <Sidebar database={database}></Sidebar>
+                </div>
+                <div className="right">
+                    <Routes></Routes>
+                </div>
+                <div className="route">
+                    <Routes>
+                        <Route path="/" element={<CheckList />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
