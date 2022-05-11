2a7fdfa8fdde5d17f3cd48685015c0d9cfb67c4c
pooriaahmadi <pooriababak444@gmail.com>
2022-05-11 09:12:40 -0400

    About us route added

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,51 @@ import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import CheckList from "./Components/CheckList/CheckList";
 import Database from "./Database";
+import AboutUs from "./Components/Aboutus/AboutUs";
 
 const App = () => {
-    const [database, setDatabase] = useState();
-    const [currentChecklist, setCurrentChecklist] = useState(false);
-    useEffect(() => {
-        const stuff = async () => {
-            const db = await idb.openDB("checkto", 1, {
-                async upgrade(db, oldVersion, newVersion, transaction) {
-                    const objectStores = ["checklists"];
-                    for (let i = 0; i < objectStores.length; i++) {
-                        try {
-                            await db.deleteObjectStore(objectStores[i]);
-                        } catch (error) {}
-                    }
-                    await Database.first_time(db);
-                },
-            });
-            setDatabase(db);
-        };
-        stuff();
-    }, []);
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header></Header>
-                <div className="left">
-                    <Sidebar
-                        database={database}
-                        currentChecklist={currentChecklist}
-                        setCurrentChecklist={setCurrentChecklist}
-                    ></Sidebar>
-                </div>
-                <div className="right">
-                    <Routes></Routes>
-                </div>
-                <div className="route">
-                    <Routes>
-                        <Route path="/" element={<CheckList />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+	const [database, setDatabase] = useState();
+	const [currentChecklist, setCurrentChecklist] = useState(false);
+	useEffect(() => {
+		const stuff = async () => {
+			const db = await idb.openDB("checkto", 1, {
+				async upgrade(db, oldVersion, newVersion, transaction) {
+					const objectStores = ["checklists"];
+					for (let i = 0; i < objectStores.length; i++) {
+						try {
+							await db.deleteObjectStore(objectStores[i]);
+						} catch (error) {}
+					}
+					await Database.first_time(db);
+				},
+			});
+			setDatabase(db);
+		};
+		stuff();
+	}, []);
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header></Header>
+				<div className="left">
+					<Sidebar
+						database={database}
+						currentChecklist={currentChecklist}
+						setCurrentChecklist={setCurrentChecklist}
+					></Sidebar>
+				</div>
+				<div className="right">
+					<Routes></Routes>
+				</div>
+				<div className="route">
+					<Routes>
+						<Route path="/" element={<CheckList />} />
+						<Route path="/aboutus" element={<AboutUs />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
