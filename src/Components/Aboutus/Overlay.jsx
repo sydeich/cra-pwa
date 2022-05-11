4e81c21722d912f657124a9356b553fab6179d61
pooriaahmadi <pooriababak444@gmail.com>
2022-05-11 09:14:02 -0400

    Overlay implementation

---
+++ src/Components/Aboutus/Overlay.jsx
@@ -0,0 +1,39 @@
+import close from "../../assets/images/arrow.svg";
+import "../../assets/scss/overlay.scss";
+const Overlay = ({
+	image,
+	name,
+	role,
+	email,
+	phone,
+	github,
+	linkedIn,
+	handleClick,
+	isActive,
+	opposite = false,
+}) => {
+	return (
+		<div
+			className={
+				"overlay" +
+				(isActive ? " active" : "") +
+				(opposite ? " opposite" : "")
+			}
+		>
+			<img src={close} onClick={handleClick} />
+			<div>
+				<div></div>
+				<h1>{name}</h1>
+				<p>{role}</p>
+			</div>
+			<div className="social-media">
+				<a href={`mailto:${email}`}>Email</a>
+				<a href={`tel:${phone}`}>Phone Number</a>
+				<a href={github}>GitHub</a>
+				<a href={linkedIn}>LinkedIn</a>
+			</div>
+		</div>
+	);
+};
+
+export default Overlay;
